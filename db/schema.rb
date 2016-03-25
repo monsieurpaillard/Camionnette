87691a045f8b6ea88db0e90e742f72aa30c00164
Romain Paillard <romain.paillard@gmail.com>
2016-03-25 18:24:25 +0000

    added truck model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325134149) do
+ActiveRecord::Schema.define(version: 20160325182356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trucks", force: :cascade do |t|
+    t.string   "brand"
+    t.integer  "year"
+    t.integer  "kilometers"
+    t.string   "type"
+    t.string   "model"
+    t.boolean  "available"
+    t.integer  "daily_price"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "trucks", ["user_id"], name: "index_trucks_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,9 +44,13 @@ ActiveRecord::Schema.define(version: 20160325134149) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "trucks", "users"
 end
